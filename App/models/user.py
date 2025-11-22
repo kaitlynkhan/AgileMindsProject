ada145c62d81bd58deb327671d1dd91b87e949dc
tlbuwi01 <tlbuwi01@gmail.com>
2025-11-21 23:52:46 -0400

    Update user.py

--- App/models/user.py
+++ App/models/user.py
@@ -2,6 +2,15 @@ from werkzeug.security import check_password_hash, generate_password_hash
 from App.database import db
 
 class User(db.Model):
+    
+    # Represents a user in the system.
+    # Attributes:
+    # ID(int) : PK
+    # username (string): unique for login
+    # password (string): hashed password
+    # role (string): Role of user (staff/admin)
+    # active_token (string): optional for authentication
+    
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), nullable=False, unique=True)
     password = db.Column(db.String(256), nullable=False)
@@ -13,10 +22,12 @@ class User(db.Model):
         "polymorphic_on": "role"
     }
 
-    def __init__(self, username, password, role="user"):
+    def __init__(self, username, password, role="user") -> None:
+        # Initializes user with username, password and role
         self.username = username
         self.role = role
         self.password = generate_password_hash(password)
 
     def check_password(self, password):
+        # checks if entered password matches the stored hash 
         return check_password_hash(self.password, password) 
