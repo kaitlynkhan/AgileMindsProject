c64d67ca105fce1b0088574a23a63b265c47f546
tlbuwi01 <tlbuwi01@gmail.com>
2025-11-22 00:15:17 -0400

    Update admin.py

--- App/models/admin.py
+++ App/models/admin.py
@@ -1,27 +1,31 @@
 
 from App.database import db
 from App.models.user import User
-
 from App.models.strategies.schedule_strategy import ScheduleStrategy
 from App.models.strategies.even_distribution import EvenDistributionStrategy
 from App.models.strategies.minimize_days import MinimizeDaysStrategy
 from App.models.strategies.balance_day_night import BalanceDayNightStrategy
+from typing import List, Optional 
 
 class Admin(User):
+
+    #Represents an admin user on the system
+    # Inherits from User and Manages staff scheduling using the strategy design pattern 
+    
     id = db.Column(db.Integer, db.ForeignKey("user.id"), primary_key=True)
     __mapper_args__ = {
         "polymorphic_identity": "admin",
     }
 
-    def __init__(self, username, password):
+    def __init__(self, username, password) -> None:
         super().__init__(username, password, "admin")
         self.schedule_strategy = None
 
     # Strategy pattern methods
-    def set_schedule_strategy(self, strategy: ScheduleStrategy):
+    def set_schedule_strategy(self, strategy: ScheduleStrategy)-> None:
         self.schedule_strategy = strategy
 
-    def generate_schedule(self, staff_list, shift_list):
+    def generate_schedule(self, staff_list, shift_list)-> List:
         if not self.schedule_strategy:
             raise ValueError("No strategy assigned")
         return self.schedule_strategy.generate(staff_list, shift_list)
